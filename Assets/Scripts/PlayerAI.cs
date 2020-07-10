d380fa49dccb9df1b84c2edc2fa6c0410f773cd5
Callum Metcalfe <metcalfewood@hotmail.com>
2020-07-10 22:44:12 +0100

    created basic playerAI that will follow waypoints

---
+++ Assets/Scripts/PlayerAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAI : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField]
+    private GameObject[] waypoints = null;
+    [SerializeField]
+    private float moveForce = 0f;
+    [SerializeField]
+    private float rotationSpeed = 0f;
+    private Vector3 moveDir;
+    private int currentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveDir * moveForce);
+    }
+
+    private void Update()
+    {
+        if (waypoints.Length > 0)
+        {
+            if (Vector3.Distance(this.transform.position, waypoints[currentWaypoint].transform.position) >= 2f)
+            {
+                Vector3 direction = waypoints[currentWaypoint].transform.position - this.transform.position;
+                direction.y = 0f;
+
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotationSpeed);
+
+                if (direction.magnitude > 0.8f)
+                {
+                    moveDir = direction.normalized;
+                }
+            }
+            else
+            {
+                moveDir = Vector3.zero;
+                currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 0.5f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(this.transform.position, radius);
+    }
+}
