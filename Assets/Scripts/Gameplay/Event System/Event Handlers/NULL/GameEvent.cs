8bdc78574eec8af86fa66592856611b6cbcbd4ba
Christopher Robertson <christopherprobertson@gmail.com>
2020-07-10 21:54:57 +0100

    - Added the basics for the event system to be used later down the line.

---
+++ Assets/Scripts/Gameplay/Event System/Event Handlers/NULL/GameEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Scriptable Object that stores all of the GameEventListeners that are subscribed to it.
+/// Following the Unity Architecture: https://unity.com/how-to/architect-game-code-scriptable-objects
+/// </summary>
+namespace LegDay.Events
+{
+    [CreateAssetMenu(fileName = "Game-Event", menuName = "ScriptableObjects/Events/GameEvent")]
+    public class GameEvent : ScriptableObject
+    {
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+        public void Raise()
+        {
+            for (int i = listeners.Count - 1; i >= 0; i--)
+                listeners[i].OnEventRaise();
+        }
+
+        public void RegisterListener(GameEventListener listener) { listeners.Add(listener); }
+        public void UnregisterListener(GameEventListener listener) { listeners.Add(listener); }
+    }
+
+}
