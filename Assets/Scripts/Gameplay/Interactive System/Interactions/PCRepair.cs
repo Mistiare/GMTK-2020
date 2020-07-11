7a342b5dd3181d680292826c090870d87cbf7c86
Christopher Robertson <christopherprobertson@gmail.com>
2020-07-11 13:03:07 +0100

    - Added the PCRepair example using the game events just to ensure that multiple types will work.

---
+++ Assets/Scripts/Gameplay/Interactive System/Interactions/PCRepair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LegDay.Interaction
+{
+    [Serializable]
+    public enum PCState { BROKEN, FIXED };
+
+    public class PCRepair : Interactable
+    {
+        [SerializeField]
+        private float repairDelay = 1.0f;
+        [SerializeField]
+        private PCState state;
+
+        public override void InteractObject()
+        {
+            base.InteractObject();
+
+            InvertState();
+            ToggleState((int)state);
+        }
+
+        private void InvertState(){ state = (state == PCState.BROKEN) ? PCState.FIXED : PCState.BROKEN; }
+
+        /// <param name="state"></param>
+        /// <remarks>Unity still doesn't support enums on UnityEvents.</remarks>
+        public void ToggleState(int state)
+        {
+            this.state = (PCState)state;
+            //Disable the fire particles or something
+        }
+    }
+}
