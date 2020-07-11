935e419b88aa29069c7c6ef1234501bff5340c87
Christopher Robertson <christopherprobertson@gmail.com>
2020-07-11 18:39:07 +0100

    - Added a custom editor button for the Character Dialogue for it to randomly say something.

---
+++ Assets/Scripts/Editor/CharacterDialogueEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using LegDay.Dialogue;
+
+namespace LegDay.Tools
+{
+    [CustomEditor(typeof(CharacterDialogue), true)]
+    public class CharacterDialogueEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            CharacterDialogue dialogue = (CharacterDialogue)target;
+
+            if (GUILayout.Button("Say Something!"))
+                dialogue.LoadRandomDialogue();
+        }
+    }
+}
