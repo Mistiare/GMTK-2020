86ab3341cd94863b34e91f35ee0485a2145df5dc
JamesB <git@jamesberry.co.uk>
2020-07-11 13:57:34 +0100

    Added setup for walking

---
+++ Assets/Scripts/IK Controllers/IKBrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKBrain : MonoBehaviour
+{
+    public bool rightFoot = true;
+
+    void Step()
+    {
+        // Switches foot to step with
+        rightFoot = !rightFoot;
+    }
+}
