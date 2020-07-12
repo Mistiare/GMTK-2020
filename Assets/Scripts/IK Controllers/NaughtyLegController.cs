a60ec793cbb3801b5ad94c66035a1e946ee6afdf
JamesB <git@jamesberry.co.uk>
2020-07-12 08:48:16 +0100

    Our boy's foot has mass

--- Assets/Scripts/IK Controllers/NaughtyLegController.cs
+++ Assets/Scripts/IK Controllers/NaughtyLegController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class NaughtyLegController : MonoBehaviour
 {
     public GameObject controller;
+    public GameObject spineController;
+    [SerializeField]
+    private float speed;
 
     void Update()
     {
@@ -19,6 +22,13 @@ public class NaughtyLegController : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             controller.transform.position = hit.point;
+            var hitNoY = new Vector3(hit.point.x, 0, hit.point.z);
+            var spineNoY = new Vector3(spineController.transform.position.x, 0, spineController.transform.position.z);
+            if (Vector3.Distance(hitNoY, spineNoY) >= 1f)
+            {
+                Vector3 directionVector = hitNoY - spineNoY;
+                spineController.transform.position += directionVector.normalized * speed;
+            }
         }
     }
 }
